d52757db4471810f47e736c2add3e482b48cdfb4
Param Shah <paramshah09@gmail.com>
2020-04-02 23:15:30 +0530

    react-chat-window init

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CustomerChat from './messenger.js';
 import ChatWindow from './chat-window.js';
+import ChatWidget from './chat-widget.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      {/* <CustomerChat /> */}
-      <ChatWindow />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        {/* <CustomerChat /> */}
+        
+        <ChatWindow /> 
+        {/* <ChatWidget /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
