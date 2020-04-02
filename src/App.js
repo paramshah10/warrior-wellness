a1d00c201c7295deac60fc00f56f8dc4ac9b1fbf
Param Shah <paramshah09@gmail.com>
2020-04-02 16:33:35 +0530

    added react-chat-window, works perfectly

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CustomerChat from './messenger.js';
+import ChatWindow from './chat-window.js';
 
 function App() {
   return (
@@ -19,6 +21,8 @@ function App() {
           Learn React
         </a>
       </header>
+      {/* <CustomerChat /> */}
+      <ChatWindow />
     </div>
   );
 }
