a6dd868a778bd181c3c8f6ee146b24ce40c72591
Param Shah <paramshah09@gmail.com>
2020-04-02 23:38:39 +0530

    figured out replying, added text box to reply

--- src/chat-window.js
+++ src/chat-window.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
-import {Launcher} from 'react-chat-window'
+import {Launcher} from 'react-chat-window';
 
+
+//class for react-chat-window
 class ChatWindow extends Component {
  
   constructor() {
     super();
     this.state = {
-      messageList: []
+      messageList: [],
+      newMessagesCount: 0,
+      isOpen: false,
     };
   }
  
@@ -14,32 +18,75 @@ class ChatWindow extends Component {
     this.setState({
       messageList: [...this.state.messageList, message]
     })
+    console.log(`New message`);
   }
  
+  _onFilesSelected(fileList) {
+    const objectURL = window.URL.createObjectURL(fileList[0]);
+    this.setState({
+      messageList: [...this.state.messageList, {
+        type: 'file', author: 'me',
+        data: {
+          url: objectURL,
+          fileName: fileList[0].name
+        }
+      }]
+    });
+  }
+
   _sendMessage(text) {
+    const newMessagesCount = this.state.isOpen ? this.state.newMessagesCount : this.state.newMessagesCount + 1;
     if (text.length > 0) {
       this.setState({
+        newMessagesCount: newMessagesCount,
         messageList: [...this.state.messageList, {
           author: 'them',
           type: 'text',
           data: { text }
         }]
       })
+      console.log(`New message: ${text}`);
     }
   }
  
+  _handleClick() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+      newMessagesCount: 0
+    });
+  }
+
   render() {
-    return (<div>
+    return (
+    <div>
       <Launcher
         agentProfile={{
           teamName: 'react-chat-window-param',
           imageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png'
         }}
         onMessageWasSent={this._onMessageWasSent.bind(this)}
+        onFilesSelected={this._onFilesSelected.bind(this)}
         messageList={this.state.messageList}
+        newMessagesCount={this.state.newMessagesCount}
+        handleClick={this._handleClick.bind(this)}
+        isOpen={this.state.isOpen}
         showEmoji
       />
-    </div>)
+      <form onSubmit={(e)=> {
+            e.preventDefault();
+            this._sendMessage(this.textArea.value);         //use _send message to reply to messages
+            this.textArea.value = '';
+        }}>
+        <div>Test the chat window by sending a message:</div>
+        <textarea
+            ref={(e) => { this.textArea = e; }}
+            className="demo-test-area--text"
+            placeholder="Write a test message...."
+        />
+        <button> Send Message! </button>
+    </form>
+    </div>
+    )
   }
 }
 
