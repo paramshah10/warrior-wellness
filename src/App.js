ced806d311ef7f7434d9f28bf03e99a1c9773157
Param Shah <paramshah09@gmail.com>
2020-04-04 04:08:21 +0530

    finally got messenger plugin to work

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import CustomerChat from './messenger.js';
 import ChatWindow from './chat-window.js';
 import ChatWidget from './chat-widget.js';
+import Messenger from './messenger.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Messenger />
         </header>
         {/* <CustomerChat /> */}
         
-        <ChatWindow /> 
+        {/* <ChatWindow />  */}
+
         {/* <ChatWidget /> */}
+
       </div>
     );
   }
