f83807105d9e8164524dbf6856e44e2f544bfcc2
Param Shah <paramshah09@gmail.com>
2020-04-05 02:33:40 +0530

    react-messenger-send-to-messenger plugin

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CustomerChat from './messenger.js';
 import ChatWindow from './chat-window.js';
 import ChatWidget from './chat-widget.js';
 import Messenger from './messenger.js'
+import MessengerSendToMessenger from 'react-messenger-send-to-messenger';
 
 class App extends Component {
   render() {
@@ -11,6 +12,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <Messenger />
+          <MessengerSendToMessenger 
+            pageId="100902811579573"
+            appId="203947964382425"
+            />
         </header>
         {/* <CustomerChat /> */}
         
