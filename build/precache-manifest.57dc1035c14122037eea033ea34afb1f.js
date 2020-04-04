daaf71e8816c8b9edc0b28781d9e101fd5e0643b
Param Shah <paramshah09@gmail.com>
2020-04-05 02:39:04 +0530

    deployment files

---
+++ build/precache-manifest.57dc1035c14122037eea033ea34afb1f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6935e683ecd082f137be71bd2ac96a0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6f9d06bc5bac2e6883d",
+    "url": "/static/css/2.27c434bd.chunk.css"
+  },
+  {
+    "revision": "31767587d8bc08e8fc22",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e6f9d06bc5bac2e6883d",
+    "url": "/static/js/2.d5c6a159.chunk.js"
+  },
+  {
+    "revision": "56f0dd3273df835662113fd2e16fe698",
+    "url": "/static/js/2.d5c6a159.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31767587d8bc08e8fc22",
+    "url": "/static/js/main.c818fcb6.chunk.js"
+  },
+  {
+    "revision": "d9eb39ca27ffe68c7213",
+    "url": "/static/js/runtime-main.62b44ee8.js"
+  },
+  {
+    "revision": "e0d2b7486b532288a980d0cd5d535340",
+    "url": "/static/media/chat-icon.e0d2b748.svg"
+  },
+  {
+    "revision": "7718b3e3ff59dac728c55e725a3b0523",
+    "url": "/static/media/logo-no-bg.7718b3e3.svg"
+  },
+  {
+    "revision": "0d062b33a8b60acb2a497afe63666b16",
+    "url": "/static/media/notification.0d062b33.mp3"
+  }
+]);
