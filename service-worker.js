27fe3ba62cd1f6b8136b563a9bdbf08902b3c74b
Param Shah <paramshah09@gmail.com>
2020-04-21 23:30:45 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/patient_dashboard/precache-manifest.bd4126047378fe2b362dbb1621188306.js"
+  "/patient_dashboard/precache-manifest.c7c6c199fd3fd4236743f08ed7d5d4a3.js"
 );
 
 self.addEventListener('message', (event) => {
