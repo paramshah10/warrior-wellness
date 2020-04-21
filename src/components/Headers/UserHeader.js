356430659d776913721174b98d57b827c202bbb5
Param Shah <paramshah09@gmail.com>
2020-04-21 23:26:16 +0530

    background picture and default form text change in profile page

--- src/components/Headers/UserHeader.js
+++ src/components/Headers/UserHeader.js
@@ -29,7 +29,7 @@ class UserHeader extends React.Component {
           style={{
             minHeight: "600px",
             backgroundImage:
-              "url(" + require("assets/img/theme/profile-cover.jpg") + ")",
+              "url(" + require("assets/img/theme/profile_background.jpeg") + ")",
             backgroundSize: "cover",
             backgroundPosition: "center top"
           }}
@@ -40,18 +40,18 @@ class UserHeader extends React.Component {
           <Container className="d-flex align-items-center" fluid>
             <Row>
               <Col lg="7" md="10">
-                <h1 className="display-2 text-white">Hello Jesse</h1>
+                <h1 className="display-2 text-white">Hello Param</h1>
                 <p className="text-white mt-0 mb-5">
                   This is your profile page. You can see the progress you've
                   made with your work and manage your projects or assigned tasks
                 </p>
-                <Button
+                {/* <Button
                   color="info"
                   href="#pablo"
                   onClick={e => e.preventDefault()}
                 >
                   Edit profile
-                </Button>
+                </Button> */}
               </Col>
             </Row>
           </Container>
