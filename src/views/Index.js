bc69c8415166de797d05d96a2a6b4fa8418109c6
Param Shah <paramshah09@gmail.com>
2020-04-24 21:57:35 +0530

    stress table sort, dynamic description boxes and tooltip

--- src/views/Index.js
+++ src/views/Index.js
@@ -1,21 +1,4 @@
-/*!
-
-=========================================================
-* Argon Dashboard React - v1.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-import React from "react";
+import React, { useState } from "react";
 
 import {
   Button,
@@ -27,25 +10,14 @@ import {
   Col,
   Input,
   CustomInput,
+  UncontrolledTooltip,
+  Collapse,
+  CardBody,
 } from "reactstrap";
 
 import Header from "components/Headers/Header.js";
 import Graphs from "./graphs.js"
 import stressIncidents from "./stress_incidents.js";
-
-class DropDown extends React.Component {
-  render() {
-      return (
-          <CustomInput type="select" defaultValue={this.props.option}>
-              <option>Work</option>
-              <option>Social</option>
-              <option>Money</option>
-              <option>Family</option>
-              <option>Other</option>
-          </CustomInput>
-      );
-  }
-};
 
 class Index extends React.Component {
   constructor(props){
@@ -54,15 +26,25 @@ class Index extends React.Component {
       stressIncidents: stressIncidents.sort((a,b) => parseInt(b.Date) - parseInt(a.Date)),
       dateSort: true,
       stressSort: true,
+      showTextBox: Array(6).fill(false),
     }
   }
 
-  onDataToggle(e){
+  onDateToggle(e){
     e.preventDefault();
     let sort = !this.state.dateSort;
     this.setState({
       dateSort: !this.state.dateSort,
-      stressIncidents: sort ? stressIncidents.sort((a,b) => parseInt(b.Date) - parseInt(a.Date)) : stressIncidents.sort((a,b) => parseInt(a.Date) - parseInt(b.Date)),
+      stressIncidents: sort ?
+        this.state.stressIncidents.sort(function (a, b) {
+          a = a.Date.split('/');
+          b = b.Date.split('/');
+          return b[2] - a[2] || b[0] - a[0] || b[1] - a[1];
+        }) : this.state.stressIncidents.sort(function (a, b) {
+          a = a.Date.split('/');
+          b = b.Date.split('/');
+          return a[2] - b[2] || a[0] - b[0] || a[1] - b[1];
+        })
     });
   }
 
@@ -71,7 +53,8 @@ class Index extends React.Component {
     let sort = !this.state.stressSort;
     this.setState({
       stressSort: !this.state.stressSort,
-      stressIncidents: sort ? stressIncidents.sort((a,b) => b.StressScore - a.StressScore) : stressIncidents.sort((a,b) => a.StressScore - b.StressScore),
+      stressIncidents: sort ? 
+        this.state.stressIncidents.sort((a,b) => b.StressScore - a.StressScore) : this.state.stressIncidents.sort((a,b) => a.StressScore - b.StressScore),
     });
   }
 
@@ -80,6 +63,7 @@ class Index extends React.Component {
   }
   
   render() {
+    let descriptions = Array(6).fill(""); //this.state.stressIncidents.Description.slice();
     return (
       <>
         <Header />
@@ -101,7 +85,7 @@ class Index extends React.Component {
                     <tr>
                       <th scope="col"><Button>Reason</Button></th>
                       <th scopr="col"><Button onClick={e => this.onStressToggle(e)}>Stress Score</Button></th>
-                      <th scope="col"><Button onClick={e => this.onDataToggle(e)}>Date</Button></th>
+                      <th scope="col"><Button onClick={e => this.onDateToggle(e)}>Date</Button></th>
                       <th scope="col"><Button>Time</Button></th>
                       <th scope="col"><Button>Description</Button></th>
                     </tr>
@@ -109,17 +93,49 @@ class Index extends React.Component {
                   <tbody>
                     {this.state.stressIncidents.map(incident => 
                       <tr>
-                        <td>
-                          <DropDown option={incident.Reason}/>
+                        <td key={incident.Reason}>
+                          <Input 
+                            type="select" 
+                            defaultValue={incident.Reason}
+                            onChange={(e) => console.log(`stress reason changed to ${e.target.value}`)}
+                          >
+                            <option>Work</option>
+                            <option>Social</option>
+                            <option>Money</option>
+                            <option>Family</option>
+                            <option>Other</option>
+                          </Input>
                         </td>
                         <td>{incident.Stress}</td>
                         <td>{incident.Date}</td>
                         <td>{incident.Time}</td>
-                        <td>
+                        <td key={incident.Index}>
                           <Input type="textarea"
-                          defaultValue={incident.Description + incident.Date}>
-                          </Input>
-                          {/* {incident.Description} */}
+                            defaultValue={`${incident.Description} on ${incident.Date}`}
+                            id={'Box'+incident.Index}
+                            plaintext={!this.state.showTextBox[incident.Index]}
+                            onDoubleClick={(e) => {
+                              e.preventDefault();
+                              this.state.showTextBox[incident.Index] = true; 
+                              this.forceUpdate();
+                            }}
+                            // onChange={(e) => {console.log(`changed to ${e.target.value}`); descriptions[incident.Index] = e.target.value;}}
+                          />
+                          <UncontrolledTooltip delay={0} placement='right' trigger="hover focus" target={'Box'+incident.Index}>
+                            Double Click Me to Edit!
+                          </UncontrolledTooltip>                          
+                          {
+                            this.state.showTextBox[incident.Index] && 
+                            <Button
+                              color='default'
+                              size="sm"
+                              onClick={(e) => {
+                                e.preventDefault();
+                                this.state.showTextBox[incident.Index] = false;
+                                this.forceUpdate();
+                              }}
+                            >Save</Button>
+                          }
                         </td>
                       </tr>
                       )}
