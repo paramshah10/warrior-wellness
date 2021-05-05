903db8eba5348e2b8eb379d89047710b016c84c3
Param Shah <paramshah09@gmail.com>
2021-05-05 14:13:22 -0700

    Changes introduction text on user profile page

--- src/components/Headers/UserHeader.js
+++ src/components/Headers/UserHeader.js
@@ -39,11 +39,10 @@ class UserHeader extends React.Component {
           {/* Header container */}
           <Container className="d-flex align-items-center" fluid>
             <Row>
-              <Col lg="7" md="10">
+              <Col lg="12" md="10">
                 <h1 className="display-2 text-white">Hello Param</h1>
                 <p className="text-white mt-0 mb-5">
-                  This is your profile page. You can see the progress you've
-                  made with your work and manage your projects or assigned tasks
+                  Welcome to VetThrive. Let us know a little about yourself.
                 </p>
               </Col>
             </Row>
